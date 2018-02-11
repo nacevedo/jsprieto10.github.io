23b19c2810bcab7dd63cd955bbd7b8fe1904754c
jsprieto10 <js.prieto10@uniandes.edu.co>
2018-02-11 01:35:29 -0500

    Update photoP.js

--- js/photoP.js
+++ js/photoP.js
@@ -1,7 +1,7 @@
 $("#profile").click(function(){
   var x = $("#profile");
   if (x.attr("src")==='img/perfil5.jpg'){
-  x.attr("src","img/perfil1.png");
+  x.attr("src","img/perfil1.PNG");
   }
   else{
   x.attr("src","img/perfil5.jpg");
