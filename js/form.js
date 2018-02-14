0c68718ed5e2a1d442b558ea0b2ffaec8e9afc2c
js.prieto <js.prieto10@uniandes.edu.co>
2018-02-13 23:07:37 -0500

    ahora si ya casi

--- js/form.js
+++ js/form.js
@@ -6,12 +6,15 @@
 
 // servidor para mandar correo de una manera más rápida
 $('#send').click(function(){
+  // obetener los inputs del formulario
   var name = $('#nombre');
   var email = $('#email');
   var phone = $('#celular');
   var message = $('#mensaje');
+  // envio del correo usando la librería
   emailjs.send("gmail","template_BZGiJMnO",{nombre: name.val(), correo: email.val(), celular: phone.val(), mensaje: message.val()})
   .then(function(response) {
+    // se ejecuta de manera sincronica esta función que vacia los elementos del arreglo
     name = $('#nombre').val('');
     email = $('#email').val('');
     phone = $('#celular').val('');
